328aa5d4f61b0d621f4ffc039c830ab779a14e92
wahyuhadi <hrahmatwahyu@gmail.com>
2020-11-12 11:13:30 +0700

    Add nodejs payload

--- apps.go
+++ apps.go
@@ -10,12 +10,17 @@ import (
 var (
 	payload = flag.String("payload", "", "Type payload  java, python, php")
 	command = flag.String("command", "", "Type command like , cat /etc/passwd")
+	ip      = flag.String("ip", "", "Ip address for rce ")
 )
 
 func main() {
 	flag.Parse()
 	if *payload == "java" {
 		readJava()
+	}
+
+	if *payload == "nodejs" {
+		readNode()
 	}
 
 	fmt.Println("Done ..")
@@ -29,6 +34,18 @@ func readJava() {
 		return
 	}
 	newContents := strings.Replace(string(data), "codeinjection", *command, -1)
+
+	fmt.Println(newContents)
+}
+
+func readNode() {
+	flag.Parse()
+	data, err := ioutil.ReadFile("DO_NOT_DELETE_THIS_FOLDER/nodejs.file")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	newContents := strings.Replace(string(data), "ipinjection", *ip, -1)
 
 	fmt.Println(newContents)
 }
